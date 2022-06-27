55275dcf65e60d8993f1f7cf88a1ac717ec3f5b0
Andrew Petersen <fespetersen1990@gmail.com>
2022-06-27 15:30:55 -0600

    updates to css and additional coding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = { advice: '' };
+
+    //below is one of many life cycle methods//
+    componentDidMount() {
+        this.fetchAdvice();
+    }
+
+    fetchAdvice = () => {
+        axios.get('https://api.adviceslip.com/advice')
+        .then((res) => {
+            const {advice} = res.data.slip;
+
+            this.setState({ advice: advice });
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+    };
+
+    render() {
+        const {advice} = this.state;
+        return (
+            <div className='app'>
+                <div className='card'>
+                    <h1 className='heading'>{advice}</h1>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
