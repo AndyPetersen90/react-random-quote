b7f22adc1d34462bca5d677b8f664c198f69e4c3
Andrew Petersen <fespetersen1990@gmail.com>
2022-06-27 15:44:04 -0600

    Added button and button functionality

--- src/App.js
+++ src/App.js
@@ -29,6 +29,10 @@ class App extends React.Component {
             <div className='app'>
                 <div className='card'>
                     <h1 className='heading'>{advice}</h1>
+                    <button className='button' onClick={this.fetchAdvice}>
+                        <span>GET MORE ADVICE</span>
+                    </button>
+
                 </div>
             </div>
         )
